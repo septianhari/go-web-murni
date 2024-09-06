41de8ec5addafcb9c785939ced87ff88402aaa3d
septianhari <aribarno86@gmail.com>
2024-09-06 23:22:21 +0700

    multiple endpoint

--- serveMux_test.go
+++ serveMux_test.go
@@ -1,6 +1,7 @@
 package go_web
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -20,6 +21,24 @@ func TestServeMux(t *testing.T) {
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func TestRequest(t *testing.T) {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, r.Method)
+		fmt.Fprintln(w, r.RequestURI)
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
 	}
 
 	err := server.ListenAndServe()
